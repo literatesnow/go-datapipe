7908cef971c6fae4ea57ab42c1d37fc3b94123ba
Chris Cuthbertson <chris.cuthbertson@lic.co.nz>
2016-07-01 11:17:14 +1200

    Style.

--- bulk/bulk.go
+++ bulk/bulk.go
@@ -1,147 +1,146 @@
 package bulk
 
 import (
-  "database/sql"
-  "strings"
-  "bytes"
-  "strconv"
+	"bytes"
+	"database/sql"
+	"strconv"
+	"strings"
 )
 
 type Bulk struct {
-  db *sql.DB //Database handle
-  tableName string
-  columns []string
+	db        *sql.DB //Database handle
+	tableName string
+	columns   []string
 
+	stmt   *sql.Stmt     //Prepared statement for bulk insert
+	buf    []interface{} //Buffer to hold values to insert
+	bufSz  int           //Size of the buffer
+	bufPos int
 
-  stmt *sql.Stmt //Prepared statement for bulk insert
-  buf []interface{} //Buffer to hold values to insert
-  bufSz int //Size of the buffer
-  bufPos int
+	ValuePtrs []interface{} //Pointer to current row buffer
+	values    []interface{} //Buffer for the current row
+	colCount  int           //Number of columns
 
-  ValuePtrs []interface{} //Pointer to current row buffer
-  values []interface{} //Buffer for the current row
-  colCount int //Number of columns
-
-  rowPos int //Position of current row
-  TotalRowCount int //Total number of rows
+	rowPos        int //Position of current row
+	TotalRowCount int //Total number of rows
 }
 
 //Appends row values to internal buffer
 func (r *Bulk) Append() (err error) {
-  //Copy row values into buffer
-  for i := 0; i < r.colCount; i++ {
-    r.buf[r.bufPos] = r.values[i]
-    r.bufPos++
-  }
+	//Copy row values into buffer
+	for i := 0; i < r.colCount; i++ {
+		r.buf[r.bufPos] = r.values[i]
+		r.bufPos++
+	}
 
-  r.rowPos++
+	r.rowPos++
 
-  //Insert rows if buffer is full
-  if r.bufPos >= r.bufSz {
-    if _, err = r.stmt.Exec(r.buf...); err != nil {
-      return err
-    }
+	//Insert rows if buffer is full
+	if r.bufPos >= r.bufSz {
+		if _, err = r.stmt.Exec(r.buf...); err != nil {
+			return err
+		}
 
-    r.TotalRowCount += r.rowPos
+		r.TotalRowCount += r.rowPos
 
-    r.bufPos = 0
-    r.rowPos = 0
-  }
+		r.bufPos = 0
+		r.rowPos = 0
+	}
 
-  return nil
+	return nil
 }
 
 //Closes any prepared statements
 func (r *Bulk) Close() {
-  if r.stmt != nil {
-    r.stmt.Close()
-  }
+	if r.stmt != nil {
+		r.stmt.Close()
+	}
 }
 
 //Writes any unsaved values from buffer to database
 func (r *Bulk) Flush() (err error) {
-  if r.bufPos > 0 {
-    buf := make([]interface{}, r.bufPos)
-    for i := 0; i < r.bufPos; i++ {
-      buf[i] = r.buf[i]
-    }
+	if r.bufPos > 0 {
+		buf := make([]interface{}, r.bufPos)
+		for i := 0; i < r.bufPos; i++ {
+			buf[i] = r.buf[i]
+		}
 
-    stmt, err := r.prepare(r.rowPos)
-    if err != nil {
-      return err
-    }
+		stmt, err := r.prepare(r.rowPos)
+		if err != nil {
+			return err
+		}
 
-    defer stmt.Close()
+		defer stmt.Close()
 
-    if _, err = stmt.Exec(buf...); err != nil {
-      return err
-    }
+		if _, err = stmt.Exec(buf...); err != nil {
+			return err
+		}
 
-    r.TotalRowCount += r.rowPos
+		r.TotalRowCount += r.rowPos
 
-    r.bufPos = 0
-    r.rowPos = 0
-  }
+		r.bufPos = 0
+		r.rowPos = 0
+	}
 
-  return nil
+	return nil
 }
 
 //Creates a bulk insert SQL prepared statement based on a number of rows
 //Uses $x for row position
 func (r *Bulk) prepare(rowCount int) (stmt *sql.Stmt, err error) {
-  var buf bytes.Buffer
+	var buf bytes.Buffer
 
-  buf.WriteString("INSERT INTO ")
-  buf.WriteString(r.tableName)
-  buf.WriteString(" (")
-  buf.WriteString(strings.Join(r.columns, ","))
-  buf.WriteString(") VALUES ")
+	buf.WriteString("INSERT INTO ")
+	buf.WriteString(r.tableName)
+	buf.WriteString(" (")
+	buf.WriteString(strings.Join(r.columns, ","))
+	buf.WriteString(") VALUES ")
 
-  pos := 1
+	pos := 1
 
-  for i := 0; i < rowCount; i++ {
-    if i > 0 {
-      buf.WriteString(",");
-    }
-    buf.WriteString("(");
-    for j := 0; j < r.colCount; j++ {
-      if j > 0 {
-        buf.WriteString(",")
-      }
-      buf.WriteString("$")
-      buf.WriteString(strconv.Itoa(pos))
-      pos++
-    }
-    buf.WriteString(")");
-  }
+	for i := 0; i < rowCount; i++ {
+		if i > 0 {
+			buf.WriteString(",")
+		}
+		buf.WriteString("(")
+		for j := 0; j < r.colCount; j++ {
+			if j > 0 {
+				buf.WriteString(",")
+			}
+			buf.WriteString("$")
+			buf.WriteString(strconv.Itoa(pos))
+			pos++
+		}
+		buf.WriteString(")")
+	}
 
-  return r.db.Prepare(buf.String())
+	return r.db.Prepare(buf.String())
 }
 
 func NewBulk(db *sql.DB, columns []string, tableName string, rowCount int) (r *Bulk, err error) {
-  r = &Bulk{
-    db: db,
-    tableName: tableName,
-    columns: columns}
+	r = &Bulk{
+		db:        db,
+		tableName: tableName,
+		columns:   columns}
 
-  r.colCount = len(columns)
+	r.colCount = len(columns)
 
-  r.values = make([]interface{}, r.colCount)
-  r.ValuePtrs = make([]interface{}, r.colCount)
+	r.values = make([]interface{}, r.colCount)
+	r.ValuePtrs = make([]interface{}, r.colCount)
 
-  for i := 0; i < r.colCount; i++ {
-      r.ValuePtrs[i] = &r.values[i]
-  }
+	for i := 0; i < r.colCount; i++ {
+		r.ValuePtrs[i] = &r.values[i]
+	}
 
-  r.bufSz = r.colCount * rowCount
-  r.bufPos = 0
-  r.rowPos = 0
+	r.bufSz = r.colCount * rowCount
+	r.bufPos = 0
+	r.rowPos = 0
 
-  r.buf = make([]interface{}, r.bufSz)
+	r.buf = make([]interface{}, r.bufSz)
 
-  if r.stmt, err = r.prepare(rowCount); err != nil {
-    return nil, err
-  }
+	if r.stmt, err = r.prepare(rowCount); err != nil {
+		return nil, err
+	}
 
-  return r, nil
+	return r, nil
 }
--- datapipe.go
+++ datapipe.go
@@ -1,192 +1,192 @@
 package main
 
 import (
-  "database/sql"
+	"database/sql"
+	"errors"
+	"fmt"
 	"log"
 	"os"
-  "fmt"
-  "time"
-	"errors"
-  "strconv"
+	"strconv"
+	"time"
 
-  _ "github.com/denisenkom/go-mssqldb"
+	_ "github.com/denisenkom/go-mssqldb"
 	_ "github.com/lib/pq"
 
-  "github.com/lic-nz/go-datapipe/bulk"
+	"github.com/lic-nz/go-datapipe/bulk"
 )
 
 type Config struct {
-  maxRowBufSz int //Maximum number of rows to buffer at a time
-  maxRowTxCommit int  //Maximum number of rows to process before committing the database transaction
+	maxRowBufSz    int //Maximum number of rows to buffer at a time
+	maxRowTxCommit int //Maximum number of rows to process before committing the database transaction
 
-  srcDbDriver string //Source database driver name
-  srcDbUri string //Source database driver URI
-  srcSelectSql string //Source database select SQL statement
+	srcDbDriver  string //Source database driver name
+	srcDbUri     string //Source database driver URI
+	srcSelectSql string //Source database select SQL statement
 
-  dstDbDriver string //Destination database driver name
-  dstDbUri string //Destination database driver URI
-  dstTableName string //Destination database table name
+	dstDbDriver  string //Destination database driver name
+	dstDbUri     string //Destination database driver URI
+	dstTableName string //Destination database table name
 }
 
 func (c *Config) Init() (err error) {
- c.maxRowBufSz, _ = c.EnvInt("MAX_ROW_BUF_SZ", 100)
- c.maxRowTxCommit, _ = c.EnvInt("MAX_ROW_TX_COMMIT", 500)
+	c.maxRowBufSz, _ = c.EnvInt("MAX_ROW_BUF_SZ", 100)
+	c.maxRowTxCommit, _ = c.EnvInt("MAX_ROW_TX_COMMIT", 500)
 
-  if c.srcDbDriver, err = c.EnvStr("SRC_DB_DRIVER"); err != nil {
-    return err
-  }
-  if c.srcDbUri, err = c.EnvStr("SRC_DB_URI"); err != nil {
-    return err
-  }
-  if c.srcSelectSql, err = c.EnvStr("SRC_DB_SELECT_SQL"); err != nil {
-    return err
-  }
+	if c.srcDbDriver, err = c.EnvStr("SRC_DB_DRIVER"); err != nil {
+		return err
+	}
+	if c.srcDbUri, err = c.EnvStr("SRC_DB_URI"); err != nil {
+		return err
+	}
+	if c.srcSelectSql, err = c.EnvStr("SRC_DB_SELECT_SQL"); err != nil {
+		return err
+	}
 
-  if c.dstDbDriver, err = c.EnvStr("DST_DB_DRIVER"); err != nil {
-    return err
-  }
-  if c.dstDbUri, err = c.EnvStr("DST_DB_URI"); err != nil {
-    return err
-  }
-  if c.dstTableName, err = c.EnvStr("DST_DB_TABLE_NAME"); err != nil {
-    return err
-  }
+	if c.dstDbDriver, err = c.EnvStr("DST_DB_DRIVER"); err != nil {
+		return err
+	}
+	if c.dstDbUri, err = c.EnvStr("DST_DB_URI"); err != nil {
+		return err
+	}
+	if c.dstTableName, err = c.EnvStr("DST_DB_TABLE_NAME"); err != nil {
+		return err
+	}
 
-  return nil
+	return nil
 }
 
 func (c *Config) EnvStr(envName string) (dst string, err error) {
-  dst = os.Getenv(envName)
-  if dst == "" {
-    err = errors.New("Missing ENV variable: " + envName)
-  } else {
-    log.Printf("%s=%s", envName, dst)
-  }
+	dst = os.Getenv(envName)
+	if dst == "" {
+		err = errors.New("Missing ENV variable: " + envName)
+	} else {
+		log.Printf("%s=%s", envName, dst)
+	}
 
-  return dst, err
+	return dst, err
 }
 
 func (c *Config) EnvInt(envName string, defaultValue int) (dst int, err error) {
-  if dst, err = strconv.Atoi(os.Getenv(envName)); err != nil {
-    dst = defaultValue
-  }
+	if dst, err = strconv.Atoi(os.Getenv(envName)); err != nil {
+		dst = defaultValue
+	}
 
-  log.Printf("%s=%d", envName, dst)
+	log.Printf("%s=%d", envName, dst)
 
-  return dst, nil
+	return dst, nil
 }
 
 func main() {
-  cfg := &Config{}
-  if err := cfg.Init(); err != nil {
-    log.Fatal(err)
-    return
-  }
+	cfg := &Config{}
+	if err := cfg.Init(); err != nil {
+		log.Fatal(err)
+		return
+	}
 
-  if err := run(cfg); err != nil {
-    log.Fatal(err)
-    return
-  }
+	if err := run(cfg); err != nil {
+		log.Fatal(err)
+		return
+	}
 }
 
 func run(cfg *Config) (err error) {
-  var srcDb, dstDb *sql.DB
+	var srcDb, dstDb *sql.DB
 
-  if srcDb, err = sql.Open(cfg.srcDbDriver, cfg.srcDbUri); err != nil {
-    return err
-  }
+	if srcDb, err = sql.Open(cfg.srcDbDriver, cfg.srcDbUri); err != nil {
+		return err
+	}
 
 	defer srcDb.Close()
 
-  if dstDb, err = sql.Open(cfg.dstDbDriver, cfg.dstDbUri); err != nil {
-    return err
-  }
+	if dstDb, err = sql.Open(cfg.dstDbDriver, cfg.dstDbUri); err != nil {
+		return err
+	}
 
 	defer dstDb.Close()
 
-  if err = clearTable(dstDb, cfg); err != nil {
-    return err
-  }
+	if err = clearTable(dstDb, cfg); err != nil {
+		return err
+	}
 
-  if err = copyTable(srcDb, dstDb, cfg); err != nil {
-    return err
-  }
+	if err = copyTable(srcDb, dstDb, cfg); err != nil {
+		return err
+	}
 
-  return nil
+	return nil
 }
 
 func clearTable(dstDb *sql.DB, cfg *Config) (err error) {
-  if _, err = dstDb.Exec("TRUNCATE TABLE " + cfg.dstTableName); err != nil {
-    return err
-  }
-  return nil
+	if _, err = dstDb.Exec("TRUNCATE TABLE " + cfg.dstTableName); err != nil {
+		return err
+	}
+	return nil
 }
 
 func copyTable(srcDb *sql.DB, dstDb *sql.DB, cfg *Config) (err error) {
-  var rows *sql.Rows
-  var columns []string
+	var rows *sql.Rows
+	var columns []string
 
-  if rows, err = srcDb.Query(cfg.srcSelectSql); err != nil {
-    return err
-  }
+	if rows, err = srcDb.Query(cfg.srcSelectSql); err != nil {
+		return err
+	}
 
-  if columns, err = rows.Columns(); err != nil {
-    return err
-  }
+	if columns, err = rows.Columns(); err != nil {
+		return err
+	}
 
-  startTime := time.Now()
+	startTime := time.Now()
 
-  i, err := copyRows(dstDb, rows, columns, cfg)
+	i, err := copyRows(dstDb, rows, columns, cfg)
 
-  duration := time.Since(startTime)
+	duration := time.Since(startTime)
 
-  log.Printf("%d rows in %s\n", i, duration.String())
+	log.Printf("%d rows in %s\n", i, duration.String())
 
-  return err
+	return err
 }
 
 func copyRows(dstDb *sql.DB, rows *sql.Rows, columns []string, cfg *Config) (rowCount int, err error) {
-  var ir *bulk.Bulk
-  var tx *sql.Tx
+	var ir *bulk.Bulk
+	var tx *sql.Tx
 
-  if ir, err = bulk.NewBulk(dstDb, columns, cfg.dstTableName, cfg.maxRowBufSz); err != nil {
-    return 0, err
-  }
+	if ir, err = bulk.NewBulk(dstDb, columns, cfg.dstTableName, cfg.maxRowBufSz); err != nil {
+		return 0, err
+	}
 
-  defer ir.Close()
+	defer ir.Close()
 
-  if tx, err = dstDb.Begin(); err != nil {
-    return 0, err
-  }
+	if tx, err = dstDb.Begin(); err != nil {
+		return 0, err
+	}
 
-  i := 1
+	i := 1
 
-  for rows.Next() {
-      rows.Scan(ir.ValuePtrs...) //Scan row values into buffer
+	for rows.Next() {
+		rows.Scan(ir.ValuePtrs...) //Scan row values into buffer
 
-      ir.Append()
+		ir.Append()
 
-      if i % cfg.maxRowTxCommit == 0 {
-        if err = tx.Commit(); err != nil {
-          return 0, err
-        }
-        if tx, err = dstDb.Begin(); err != nil {
-          return 0, err
-        }
+		if i%cfg.maxRowTxCommit == 0 {
+			if err = tx.Commit(); err != nil {
+				return 0, err
+			}
+			if tx, err = dstDb.Begin(); err != nil {
+				return 0, err
+			}
 
-        fmt.Print(".")
-      }
+			fmt.Print(".")
+		}
 
-      i++
-  }
+		i++
+	}
 
-  ir.Flush();
+	ir.Flush()
 
-  if err = tx.Commit(); err != nil {
-    return 0, err
-  }
+	if err = tx.Commit(); err != nil {
+		return 0, err
+	}
 
-  fmt.Println()
+	fmt.Println()
 
-  return ir.TotalRowCount, nil
+	return ir.TotalRowCount, nil
 }
